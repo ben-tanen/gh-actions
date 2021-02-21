02de9fdf163484c95e3dcfa4a01a429141706974
Ben Tanen <ben-tanen@gmail.com>
2021-02-20 23:19:41 -0700

    added title to plot (so changes appear)

--- scripts/simple.R
+++ scripts/simple.R
@@ -4,5 +4,6 @@ library(dplyr)
 mtcars %>% 
     ggplot() + 
     geom_point(aes(x = mpg, y = carb)) + 
+    labs(title = format(Sys.time(), "%Y-%m-%d, %H:%M"))
     ggsave(file = "img/plot.png", device = "png", 
            width = 5, height = 5, units = "in")
